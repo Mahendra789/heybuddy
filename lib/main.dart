db45d36db9d1765702a0dab0ef8870d0966af027
Mahendra789 <ahada.mahendra@gmail.com>
2021-01-18 19:09:43 +0530

    basic setup

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import './screens/chat_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Hey Buddy',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: ChatScreen());
+  }
+}
